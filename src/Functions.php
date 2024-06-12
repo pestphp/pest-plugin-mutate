62d925a93679d4197397a37f444f7d28423ba270
Sandro Gehri <sandrogehri@gmail.com>
2024-06-12 22:55:47 +0200

    allow mutate() helper in test files

--- src/Functions.php
+++ src/Functions.php
@@ -1,10 +1,13 @@
 <?php
 
 declare(strict_types=1);
-
+use Pest\Mutate\Contracts\MutationTestRunner;
 use Pest\Mutate\Repositories\ConfigurationRepository;
 use Pest\Mutate\Support\Configuration\GlobalConfiguration;
+use Pest\PendingCalls\BeforeEachCall;
+use Pest\Support\Backtrace;
 use Pest\Support\Container;
+use Pest\TestSuite;
 
 // @codeCoverageIgnoreStart
 if (! function_exists('mutate')) {
@@ -15,6 +18,16 @@ if (! function_exists('mutate')) {
      */
     function mutate(string $profile = 'default'): GlobalConfiguration
     {
+        try {
+            if (! str_ends_with(Backtrace::testFile(), 'Pest.php')) {
+                Container::getInstance()->get(MutationTestRunner::class)->enable(); // @phpstan-ignore-line
+
+                (new BeforeEachCall(TestSuite::getInstance(), Backtrace::testFile(), fn () => null))->only(); // @phpstan-ignore-line
+            }
+        } catch (Throwable) { // @phpstan-ignore-line
+            // @ignoreException
+        }
+
         return Container::getInstance()->get(ConfigurationRepository::class)->globalConfiguration($profile); // @phpstan-ignore-line
     }
 }
