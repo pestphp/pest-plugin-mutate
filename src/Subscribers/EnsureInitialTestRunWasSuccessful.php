3a785fabd9aa1d2ab5e89a467c0f35ae7ac42794
Sandro Gehri <sandrogehri@gmail.com>
2024-09-13 15:51:43 +0200

    Calculate mutation test timeout based on the initial test suite duration

--- src/Subscribers/EnsureInitialTestRunWasSuccessful.php
+++ src/Subscribers/EnsureInitialTestRunWasSuccessful.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Pest\Mutate\Subscribers;
 
+use Pest\Mutate\Repositories\TelemetryRepository;
+use Pest\Support\Container;
 use PHPUnit\Event\Application\Finished;
 use PHPUnit\Event\Application\FinishedSubscriber;
 
@@ -12,5 +14,11 @@ use PHPUnit\Event\Application\FinishedSubscriber;
  */
 final class EnsureInitialTestRunWasSuccessful implements FinishedSubscriber
 {
-    public function notify(Finished $event): void {}
+    public function notify(Finished $event): void
+    {
+        // @phpstan-ignore-next-line
+        Container::getInstance()->get(TelemetryRepository::class)->initialTestSuiteDuration(
+            $event->telemetryInfo()->durationSinceStart()
+        );
+    }
 }
